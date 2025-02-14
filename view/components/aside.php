b47151b5be1b39c5ea78d5d24272e911e6fb9685
danieldmota <danieldourado2102@gmail.com>
2025-02-14 16:47:55 -0400

    adicionando usuario na sidebar

--- view/components/aside.php
+++ view/components/aside.php
@@ -3,5 +3,6 @@
         <li><a href="../pages/home.php">HOME</a></li>
         <li><a href="../pages/categorias.php">CATEGORIAS</a></li>
         <li><a href="../pages/produtos.php">PRODUTOS</a></li>
+        <li><a href="../pages/usuario.php">USUÁRIO</a></li>
     </ul>
 </aside>
